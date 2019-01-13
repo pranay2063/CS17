ac50aec6a0cac2d6f02814e1aea1d3f39530092f
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-01-13 17:26:58 +0530

    Multi-Level Inheritance

---
+++ OOPS/Inheritance/MultiLevel/C/First.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class A{
+	
+	public:
+	void display(){
+		cout<<"Base class data"<<endl;
+	}
+	
+};
+
+class B : public A {
+	
+};
+
+class C : public B {
+	
+};
+
+int main() {
+	C obj;
+	obj.display();
+	return 0;
+}
