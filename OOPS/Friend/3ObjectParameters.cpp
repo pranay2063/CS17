4b8d6ca48628fbdeadaf80278e03f55c1aa487b7
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-01-14 08:22:44 +0530

    Object as parameters of a friend function

---
+++ OOPS/Friend/3ObjectParameters.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class A{
+	int x;
+	public:
+	void set(){
+		x = 69;
+	}
+	void display(){
+		cout<<"x is : "<<x<<endl;
+	}
+	
+	friend void update(A);
+};
+
+void update(A a){
+	//All data members and methods can be accessed through objects in friend function
+	a.x = 89;
+	cout<<"value of x inside friend : "<<a.x<<endl;
+}
+
+int main() {
+	A obj;
+	//obj.x = 9; ->compilation error as x is private
+	obj.set();
+	obj.display(); //x is : 69
+	update(obj);   //value of x inside friend : 89   
+	obj.display(); //x is : 69
+	return 0;
+}
