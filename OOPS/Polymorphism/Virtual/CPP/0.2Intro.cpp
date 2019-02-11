c05fea2f3142d9113d08d12bb6c9c7ec5e4f312c
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-02-11 22:34:16 +0530

    Base class pointer to virtual + No virtual

---
+++ OOPS/Polymorphism/Virtual/CPP/0.2Intro.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+class base{
+	public:
+	void show() { cout<<"Welcome to Base!"<<endl; }
+};
+
+class derived : public base{
+	public:
+	void show() { cout<<"Welcome to Derived!"<<endl; }
+};
+
+int main() {
+	base *objb = new derived; //Compile-time polymorphism
+	objb->show(); //Welcome to Base!
+	return 0;
+}
