4dd819b90c383adcb7b3d6823956fe94c3859e00
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-02-11 22:30:13 +0530

    Base class pointer to derived calls virtual func

---
+++ OOPS/Polymorphism/Virtual/CPP/1BasePointer.cpp
@@ -0,0 +1,20 @@
+//Runtime Polymorphism - The decision to call an overridden function is taken at runtime
+
+#include <iostream>
+using namespace std;
+
+class base{
+	public:
+	virtual void show() { cout<<"Welcome to Base!"<<endl; }
+};
+
+class derived : public base{
+	public:
+	void show() { cout<<"Welcome to Derived!"<<endl; }
+};
+
+int main() {
+	base *objb = new derived; //Runtime Polymorphism
+	objb->show(); //Welcome to Derived!
+	return 0;
+}
