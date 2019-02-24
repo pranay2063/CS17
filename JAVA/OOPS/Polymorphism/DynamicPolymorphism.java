f2f64a494c3531adef14cc0d3cdd445be6550bcb
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-02-24 21:44:08 +0530

    Create DynamicPolymorphism.java

---
+++ JAVA/OOPS/Polymorphism/DynamicPolymorphism.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Animal{
+	void sound(){
+		System.out.println("Default Animal Sound");
+	}
+}
+
+class Dog extends Animal
+{
+	
+	void sound(){
+		System.out.println("Bark");
+	}
+	
+}	
+
+/* Name of the class has to be "Main" only if the class is public. */
+class DynamicPolymorphism
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Animal obj = new Dog(); //Instance of class 'Animal' referring to instance of 'Dog'
+		obj.sound(); //Method is decided at runtime (dynamic)
+	}
+}
