b2bd269e79a2e269af0bea0035399f4f3df12004
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-08-19 06:34:30 +0530

    Thread class - GetId(), GetName(), SetName()

--- JAVA/Multithreading/MultithreadingGetIdSetName.java
+++ JAVA/Multithreading/MultithreadingGetIdSetName.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class MultithreadingInterface extends Thread{
+class MultithreadingClass extends Thread{
     private static int counter = 0;
     @Override
     public void run(){
@@ -19,34 +19,12 @@ class MultithreadingInterface extends Thread{
     }
 }
 
-class MultithreadingGetIdSetName { 
+class MultithreadingGetIdSetName {
     public static void main(String[] args){
         for(int i = 0; i < 5; ++i){
-            MultithreadingInterface obj = new MultithreadingInterface();
+            MultithreadingClass obj = new MultithreadingClass ();
             obj.setName("Thread "+i);
             obj.start();
-            /*
-            Counter for Thread (Name,Id) (Thread 1,13) now is 0
-            Counter for Thread (Name,Id) (Thread 0,12) now is 0
-            Incrementing counter to 2
-            Counter for Thread (Name,Id) (Thread 4,16) now is 0
-            Incrementing counter to 3
-            Counter for Thread (Name,Id) (Thread 3,15) now is 0
-            Incrementing counter to 4
-            Counter for Thread (Name,Id) (Thread 2,14) now is 0
-            Incrementing counter to 5
-            Incrementing counter to 1
-            Counter for Thread (Name,Id) (Thread 0,12) now is 5
-            Counter for Thread (Name,Id) (Thread 1,13) now is 5
-            Incrementing counter to 7
-            Counter for Thread (Name,Id) (Thread 4,16) now is 5
-            Counter for Thread (Name,Id) (Thread 2,14) now is 5
-            Incrementing counter to 9
-            Counter for Thread (Name,Id) (Thread 3,15) now is 5
-            Incrementing counter to 8
-            Incrementing counter to 6
-            Incrementing counter to 10
-             */
         }
     }
 }
